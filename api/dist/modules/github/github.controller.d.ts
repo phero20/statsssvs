ba7facab5223e9a103671304059364cf7e0542f8
Feroz <mdferozahmed27156@gmail.com>
2026-03-03 19:18:27 +0530

    updated api/dist/modules/github/github.controller.d.ts

---
+++ api/dist/modules/github/github.controller.d.ts
@@ -0,0 +1,67 @@
+import { Elysia } from "elysia";
+export declare const github: (app: Elysia) => Elysia<"", {
+    decorator: {};
+    store: {};
+    derive: {};
+    resolve: {};
+}, {
+    typebox: {};
+    error: {};
+}, {
+    schema: {};
+    standaloneSchema: {};
+    macro: {};
+    macroFn: {};
+    parser: {};
+    response: {};
+} & {
+    schema: {};
+    standaloneSchema: {};
+    macro: {};
+    macroFn: {};
+    parser: {};
+    response: {};
+}, {
+    "full-stats": {
+        ":username": {
+            get: {
+                body: unknown;
+                params: {
+                    username: string;
+                };
+                query: unknown;
+                headers: unknown;
+                response: {
+                    200: import("../../shared/utils/response").ApiResponse<import("./github.types").UserStatsResponse>;
+                    422: {
+                        type: "validation";
+                        on: string;
+                        summary?: string;
+                        message?: string;
+                        found?: unknown;
+                        property?: string;
+                        expected?: string;
+                    };
+                };
+            };
+        };
+    };
+}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+} & {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+}>;
