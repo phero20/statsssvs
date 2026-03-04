4d0a9871862b268cdea452a8094d7a907922ac3d
Feroz <mdferozahmed27156@gmail.com>
2026-03-04 12:06:24 +0530

    updated api/dist/shared/middlewares/logger.d.ts

---
+++ api/dist/shared/middlewares/logger.d.ts
@@ -0,0 +1,31 @@
+import { Elysia } from "elysia";
+export declare const logger: Elysia<"", {
+    decorator: {};
+    store: {};
+    derive: {
+        readonly _startTime: number;
+    };
+    resolve: {};
+}, {
+    typebox: {};
+    error: {};
+}, {
+    schema: {};
+    standaloneSchema: {};
+    macro: {};
+    macroFn: {};
+    parser: {};
+    response: {};
+}, {}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+}>;
