50ff5a78bf049bce9a27090c6998ebb0f2ff292a
Feroz <mdferozahmed27156@gmail.com>
2026-03-04 12:06:24 +0530

    updated api/dist/shared/middlewares/cache.d.ts

---
+++ api/dist/shared/middlewares/cache.d.ts
@@ -0,0 +1,31 @@
+import { Elysia } from "elysia";
+export declare const cache: (options?: {
+    ttl?: number;
+}) => Elysia<"", {
+    decorator: {};
+    store: {};
+    derive: {};
+    resolve: {};
+}, {
+    typebox: {};
+    error: {};
+}, {
+    schema: {};
+    standaloneSchema: {};
+    macro: {};
+    macroFn: {};
+    parser: {};
+    response: {};
+}, {}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+}>;
