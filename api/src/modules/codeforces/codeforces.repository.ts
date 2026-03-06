31ede88fd054a38556110ad06516b88d7187914c
Feroz <mdferozahmed27156@gmail.com>
2026-03-06 12:06:54 +0530

    updated api/src/modules/codeforces/codeforces.repository.ts

--- api/src/modules/codeforces/codeforces.repository.ts
+++ api/src/modules/codeforces/codeforces.repository.ts
@@ -1,10 +1,6 @@
 import { codeforcesClient } from "@/modules/codeforces/codeforces.client";
 import { CodeforcesUserResponse } from "@/modules/codeforces/codeforces.types";
 
-/**
- * Repository Layer
- * Responsibility: Fetch raw data from the Codeforces REST API
- */
 export const getUserStats = async (
   username: string,
 ): Promise<CodeforcesUserResponse> => {
