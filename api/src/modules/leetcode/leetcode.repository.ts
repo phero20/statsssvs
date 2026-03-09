487d04c1f88239f5bba529470100c8bbb244c298
Feroz <mdferozahmed27156@gmail.com>
2026-03-09 12:07:40 +0530

    updated api/src/modules/leetcode/leetcode.repository.ts

--- api/src/modules/leetcode/leetcode.repository.ts
+++ api/src/modules/leetcode/leetcode.repository.ts
@@ -1,16 +1,29 @@
 import { leetcodeClient } from "@/modules/leetcode/leetcode.client";
-import { GET_LEETCODE_USER_STATS_QUERY } from "@/modules/leetcode/leetcode.queries";
-import { LeetcodeGraphqlResponse } from "@/modules/leetcode/leetcode.types";
+import {
+  AlfaProfileResponse,
+  AlfaContestResponse,
+  AlfaBadgesResponse,
+  AlfaSolvedResponse,
+  AlfaLanguageResponse,
+  AlfaCalendarResponse,
+} from "@/modules/leetcode/leetcode.types";
 
 /**
  * Repository Layer
- * Responsibility: Data retrieval strategy (External API, Cache, DB)
+ * Responsibility: Fetch raw data from the external API
  */
-export const getUserStats = async (
-  username: string,
-): Promise<LeetcodeGraphqlResponse> => {
-  return await leetcodeClient.graphql<LeetcodeGraphqlResponse>(
-    GET_LEETCODE_USER_STATS_QUERY,
-    { username },
-  );
+export const getUserStats = async (username: string) => {
+  const [profile, solved, contest, badges, language, calendar] =
+    await Promise.all([
+      leetcodeClient.get<AlfaProfileResponse>(`/${username}`),
+      leetcodeClient.get<AlfaSolvedResponse>(`/${username}/solved`),
+      leetcodeClient.get<AlfaContestResponse>(`/${username}/contest`),
+      leetcodeClient.get<AlfaBadgesResponse>(`/${username}/badges`),
+      leetcodeClient.get<AlfaLanguageResponse>(`/${username}/language`),
+      leetcodeClient.get<AlfaCalendarResponse>(
+        `/${username}/calendar?year=${new Date().getFullYear()}`,
+      ),
+    ]);
+
+  return { profile, solved, contest, badges, language, calendar };
 };
