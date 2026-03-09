f332edcf49ba3426ae1f1df8a2e0ba0d02e3ec63
Feroz <mdferozahmed27156@gmail.com>
2026-03-09 12:07:39 +0530

    updated api/src/modules/github/github.repository.ts

--- api/src/modules/github/github.repository.ts
+++ api/src/modules/github/github.repository.ts
@@ -2,14 +2,9 @@ import { githubClient } from "@/modules/github/github.client";
 import { GET_USER_STATS_QUERY } from "@/modules/github/github.queries";
 import { GithubGraphqlResponse } from "@/modules/github/github.types";
 
-/**
- * Repository Layer
- * Responsibility: Data retrieval strategy (External API, Cache, DB)
- */
 export const getUserStats = async (
   username: string,
 ): Promise<GithubGraphqlResponse> => {
-  // In the future, we could check a Local DB here before hitting the API
   return await githubClient.graphql<GithubGraphqlResponse>(
     GET_USER_STATS_QUERY,
     { login: username },
