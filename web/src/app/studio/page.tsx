83ae138e21317e05f1f9bc01811cb2c75ce0f034
Feroz <mdferozahmed27156@gmail.com>
2026-03-10 12:07:50 +0530

    updated web/src/app/studio/page.tsx

--- web/src/app/studio/page.tsx
+++ web/src/app/studio/page.tsx
@@ -1,10 +1,12 @@
 import { PlatformsGrid } from "./_components/PlatformsGrid";
+import Preview from "./_components/features/Preview";
 
 
 export default function StudioPage() {
   return (
-    <main className="flex min-h-screen items-center justify-center py-24">
+    <main className="flex flex-col min-h-screen items-center justify-center py-24 gap-24">
       <PlatformsGrid />
+      <Preview />
    </main>
   );
 }
